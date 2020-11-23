6f122e08ea2ae2ea459c84e4f39ab75c12a96470
Manpreet Kaur <kaurman2305@gmail.com>
2020-11-23 19:16:11 +0530

    Cascade Delete Task after owner gets removed

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const { default: validator } = require('validator')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -101,6 +102,14 @@ userSchema.pre('save', async  function (next) {
 
     next()
 } )
+
+userSchema.pre('remove', async function (next) {
+    const user = this
+
+    await Task.deleteMany({owner: user._id})
+
+    next()
+})
 
 const User = mongoose.model('User',userSchema);
 
