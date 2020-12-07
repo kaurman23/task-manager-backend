6b768d4a61d84ae8bfaa7370a0a60b3514e8ffa6
Manpreet Kaur <kaurman2305@gmail.com>
2020-12-07 20:47:39 +0530

    Set environment variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { default: validator } = require('validator');
 
-mongoose.connect('mongodb://127.0.0.1:27018/task-manager-api',{ 
+mongoose.connect(process.env.MONGODB_URL,{ 
     useNewUrlParser: true, 
     useUnifiedTopology:true,
     useFindAndModify:false,
