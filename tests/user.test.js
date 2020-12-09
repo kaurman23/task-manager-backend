20d83d630eb11761dcb276e4804072727a0d5c34
Manpreet Kaur <kaurman2305@gmail.com>
2020-12-09 18:47:07 +0530

    Create a test suit for user to check log in and register

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,18 @@
 const request = require('supertest')
+const { response } = require('../src/app')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: "Mike",
+    email: "monster@inc.com",
+    password: "whatever123"
+}
+
+beforeEach( async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should register user', async () => {
     await request(app).post('/users').send({
@@ -7,4 +20,20 @@ test('Should register user', async () => {
         email:'x@y.com',
         password:'whatever123'
     }).expect(201)
+})
+
+test('Should log in existing users', async () => {
+    await request(app).post('/users/login').send({
+        name: "Mike",
+        email: "monster@inc.com",
+        password: "whatever123"
+    }).expect(200)
+})
+
+test('Should not log in non-existing user', async () => {
+    await request(app).post('/users/login').send({
+        name: "Mike",
+        email: "monster@inc.com",
+        password: "whatever"
+    }).expect(400)
 })
